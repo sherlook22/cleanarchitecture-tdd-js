21188752a6c4aec249bee386965004eda17367e6
Gonzalo Re <gonzalore01@gmail.com>
2021-05-13 17:29:57 -0300

    ensure EmailValidator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -18,7 +18,13 @@ describe('Email Validator', () => {
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
-    const isEmailValid = sut.isValid('valid_email@email.com')
+    const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
